f21637aa029f1949b212a458a427890b5eb693bf
ValentynaSavchenko <149622417+ValentynaSavchenko@users.noreply.github.com>
2024-05-22 22:50:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,45 @@ def to_search():
     for item in data:
         tree.insert('', 'end', values = item)
 
-
 app_name = Label(frame_up, text="Phonebook", height=1, font=('Verdana 17 bold'), bg=co2, fg=co0)
 app_name.place(x=5, y=5)
+
+l_name = Label(frame_down, text='Name *', width=20, height=1, font=('Ivy 10'), bg=co0, anchor=NW)
+l_name.place(x=10, y=20)
+e_name = Entry(frame_down, width=25, justify='left', highlightthickness=1, relief='solid')
+e_name.place(x=80, y=20)
+
+l_gender = Label(frame_down, text='Gender *', width=20, height=1, font=('Ivy 10'), bg=co0, anchor=NW)
+l_gender.place(x=10, y=50)
+c_gender = ttk.Combobox(frame_down, width=22)
+c_gender['values'] =['', 'F', 'M'] 
+c_gender.place(x=80, y=50)
+
+l_telephone = Label(frame_down, text='Telephone *', height=1, font=('Ivy 10'), bg=co0, anchor=NW)
+l_telephone.place(x=10, y=80)
+e_telephone = Entry(frame_down, width=25, justify='left', highlightthickness=1, relief='solid')
+e_telephone.place(x=80, y=80)
+
+l_email = Label(frame_down, text='Email *', height=1, font=('Ivy 10'), bg=co0, anchor=NW)
+l_email.place(x=10, y=110)
+e_email = Entry(frame_down, width=25, justify='left', highlightthickness=1, relief='solid')
+e_email.place(x=80, y=110)
+
+b_search = Button(frame_down, text='Search', height=1, bg=co2, fg=co0, font=('Ivy 8 bold'), command=to_search)
+b_search.place(x=290, y=20)
+e_search = Entry(frame_down, width=16, justify='left', font=('Ivy', 11), highlightthickness=1, relief='solid')
+e_search.place(x=347, y =20)
+
+b_view = Button(frame_down, text='View', width=10, height=1, bg=co2, fg=co0, font=('Ivy 8 bold'), command=show)
+b_view.place(x=290, y=50)
+
+b_add = Button(frame_down, text='Add', width=10, height=1, bg=co2, fg=co0, font=('Ivy 8 bold'), command=insert)
+b_add.place(x=400, y=50)
+
+b_update = Button(frame_down, text='Update', width=10, height=1, bg=co2, fg=co0, font=('Ivy 8 bold'), command=to_update)
+b_update.place(x=400, y=80)
+
+b_delete = Button(frame_down, text='Delete', width=10, height=1, bg=co2, fg=co0, font=('Ivy 8 bold'), command=to_remove)
+b_delete.place(x=400, y=110)
+
 window.mainloop()
